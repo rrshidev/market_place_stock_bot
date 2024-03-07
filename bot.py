e52b3535e19a46fef7b5efda722a170a6fda02ab
rrshidev <rrshidev@gmail.com>
2024-03-07 21:19:52 +0300

    bot can add photo of products

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ from stock import get_stock
 from markups import menu_markup, menu_markup_names_of_products, menu_markup_product
 from Secrets.config import telegram_token, sheet_id
 from utils.long_message import long_message, send_long_message
+from Secrets.config import path_to_photos
+
 
 
 API_TOKEN = telegram_token
@@ -69,12 +71,33 @@ async def process_callback(callback_query: types.CallbackQuery):
     if callback_query.data.startswith('product|'):
         product_index = callback_query.data.split('|')[1]
         chat_id = callback_query.message.chat.id
-        product_data = get_product_data(product_index=product_index)
-        await bot.send_message(
-            chat_id=chat_id,
-            text=product_data,
-            reply_markup=menu_markup()
-        )
+        product_data_list = await get_product_data(product_index=product_index, flag=False)
+        product_data = product_data_list[0]
+        flag = product_data[1]
+        print(flag)
+        photo_name = product_data_list[2]
+        if flag:
+            with open(f'{path_to_photos}{photo_name}', 'rb') as f:
+                photo = f.read()
+
+                await bot.send_message(
+                    chat_id=chat_id,
+                    text=product_data,
+                )
+                await bot.send_photo(
+                    chat_id=chat_id,
+                    photo=photo, 
+                    reply_markup=menu_markup(),
+                    
+                )
+        else:
+            await bot.send_message(
+                chat_id=chat_id,
+                text=product_data,
+                reply_markup=menu_markup(),
+
+            )
+
 
     await bot.send_message(
         chat_id=chat_id,
--- getRequests/getProductData.py
+++ getRequests/getProductData.py
@@ -1,8 +1,10 @@
+import os
+
 from getRequests.getRequest import get_service
-from Secrets.config import sheet_id
+from Secrets.config import sheet_id, path_to_photos
 
 
-def get_product_data(product_index):
+async def get_product_data(product_index, flag):
 
     sheet_name = 'Склад'
     
@@ -18,14 +20,20 @@ def get_product_data(product_index):
     product_names = tableData[0]
     product_balance = tableData[1]
     product_size = tableData[3]
-   
     product_name = product_names[int(product_index)]
-    print(product_name)
-   
     product_message = ''
+    product_list = []
     cnt = 0
     for product in product_names:
         if product == product_name:
-            product_message += f'{product_name} - остаток: {product_balance[cnt]} - разымер: {product_size[cnt]}\n'
+            product_message += f'{product_name} - остаток: {product_balance[cnt]} - размер: {product_size[cnt]}\n'
         cnt += 1
-    return product_message
+
+    files_list = os.listdir(path_to_photos)
+    photo_name = product_name + '.jpg'
+    if photo_name in files_list:
+        flag = True
+    product_list.append(product_message)
+    product_list.append(flag)
+    product_list.append(photo_name)
+    return product_list
